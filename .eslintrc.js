db77a98e7f6023e28aef7a559c2552d5f702790e
NurGeo <anzpro@gmail.com>
2024-04-02 15:22:30 +0500

    Начальная инициализация, написаны цели и нефункциональные требования к продукту

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'class-methods-use-this': 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-namespace': 'off',
+  },
+};
